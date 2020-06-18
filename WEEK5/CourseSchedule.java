74447ca5b73fb4612bde36ac107ee6fd4208974f
Abhishek Surve <abhishek.sur@media.net>
2020-06-18 18:43:04 +0530

    W5:Q1: Course Schedule

--- WEEK5/CourseSchedule.java
+++ WEEK5/CourseSchedule.java
@@ -2,6 +2,7 @@ public class CourseSchedule {
     /**
      * Question : https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/538/week-5-may-29th-may-31st/3344/
      */
+
     class Solution {
         public boolean canFinish(int numCourses, int[][] prerequisites) {
             int[] inDegree = new int[numCourses];
@@ -31,6 +32,49 @@ public class CourseSchedule {
                 }
             }
             return count == numCourses;
+        }
+    }
+
+
+    class Solution {
+        List<Integer>[] graph;
+        boolean[] visited;
+        boolean[] marked;
+
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            graph = new ArrayList[numCourses];
+
+            for (int i = 0; i < numCourses; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            for (int i = 0; i < prerequisites.length; i++) {
+                graph[prerequisites[i][0]].add(prerequisites[i][1]);
+            }
+
+            visited = new boolean[numCourses];
+            marked = new boolean[numCourses];
+
+            for (int i = 0; i < numCourses; i++) {
+                if (!visited[i]) {
+                    if (isCyclic(i)) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private boolean isCyclic(int i) {
+            visited[i] = true;
+
+            for (Integer v : graph[i]) {
+                if (!visited[v]) {
+                    if (isCyclic(v)) return true;
+                } else if (!marked[v]) return true;
+            }
+
+            marked[i] = true;
+            return false;
         }
     }
 }
